5393e7f8412f1a1f71042673385b10a290457d9c
Ashutosh Prasad <ashutoshprasad2427@gmail.com>
2024-06-12 06:16:03 +0530

    initial commit and project setup with Hello World

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import FormUi from "./components/FormUi";
+
+function App() {
+  return (
+    <>
+      <FormUi />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/FormUi.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const FormUi = () => {
+  return (
+    <div>
+      <h1 className="p-10 bg-purple-500 text-4xl font-semibold text-white">
+        Hello World
+      </h1>
+    </div>
+  );
+};
+
+export default FormUi;
