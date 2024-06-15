eaac2ad1008b995406d70545e7ca7d4b262b45b9
Ashutosh Prasad <ashutoshprasad2427@gmail.com>
2024-06-15 08:22:28 +0530

    final

--- src/components/FormUi.jsx
+++ src/components/FormUi.jsx
@@ -4,6 +4,25 @@ const FormUi = () => {
   const [amt, setamt] = useState(0);
   const [rate, setRate] = useState(0);
   const [time, setTime] = useState(0);
+  const [simpleInterest, setSimpleInterest] = useState(0);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (amt === 0 || rate === 0 || time === 0) {
+      alert("Zero in not a valid input");
+    } else {
+      const simpleInterest = (amt * rate * time) / 100;
+      console.log(simpleInterest);
+      setSimpleInterest(parseInt(simpleInterest));
+    }
+  };
+  const handleReset = (e) => {
+    e.preventDefault();
+    setamt(0);
+    setRate(0);
+    setTime(0);
+    setSimpleInterest(0);
+  };
   return (
     <>
       <div className="  rounded p-5 bg-white">
@@ -13,9 +32,12 @@ const FormUi = () => {
         <p className="text-sm capitalize text-gray-800 font-semibold text-left m-4 ml-0 mt-0">
           calculate your simple interest easily
         </p>
-        <div className="h-32 m-5 ml-0 mr-0 bg-indigo-400 text-white flex flex-col justify-center items-center">
-          <i className="fa-solid fa-indian-rupee-sign text-5xl"></i>
-          <p className="capitalize">total simple interest</p>
+        <div className="h-32 m-5 ml-0 mr-0 bg-indigo-800 rounded-lg text-white flex flex-col justify-center items-center">
+          <span>
+            <i className="fa-solid fa-indian-rupee-sign text-3xl">
+              &nbsp;{simpleInterest}/-
+            </i>
+          </span>
         </div>
         <form>
           <div className="m-4 ml-0 mr-0">
@@ -68,12 +90,16 @@ const FormUi = () => {
             />
           </div>
           <div className="flex m-4 ml-0 mr-0 gap-2">
-            <button className="bg-black text-white p-2 w-full border-2 border-black hover:bg-gray-800 transition-all duration-300 ease-in-out rounded-md font-bold tracking-widest">
+            <button
+              className="bg-black text-white p-2 w-full border-2 border-black hover:bg-gray-800 transition-all duration-300 ease-in-out rounded-md font-bold tracking-widest"
+              onClick={handleSubmit}
+            >
               calculate
             </button>
             <button
               type="reset"
               className="bg-black text-white p-2 w-full border-2 border-black hover:bg-gray-800 transition-all duration-300 ease-in-out rounded-md font-bold tracking-widest"
+              onClick={handleReset}
             >
               reset
             </button>
